164d34e2933bc92b8668a994b2e876fb44d2a814
SHLIM1103 <gusl5525@gmail.com>
2020-12-29 20:54:28 +0900

    강의자료 추가

--- src/main/java/com/example/demo/sym/web/TeacherController.java
+++ src/main/java/com/example/demo/sym/web/TeacherController.java
@@ -1,8 +1,25 @@
 package com.example.demo.sym.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class TeacherController {
+import com.example.demo.cmm.enm.Messenger;
+import com.example.demo.sym.service.Teacher;
+import com.example.demo.sym.service.TeacherService;
 
+@RestController
+@RequestMapping("/teachers")
+public class TeacherController {
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+	@Autowired TeacherService teacherService;
+
+	@PostMapping("")
+	public Messenger register(Teacher teacher){
+		logger.info("등록하려는 교강사 정보: " + teacher.toString());
+		return (teacherService.register(teacher) == 1) ? Messenger.SUCCESS : Messenger.FAILURE;
+	}
 }
--- src/main/resources/desc/version.md
+++ src/main/resources/desc/version.md
@@ -5,3 +5,5 @@ develop-0.1.5 : 2020-12-23 tiles 설정, include 설정
 develop-0.1.6 : 2020-12-24 게시판 이름 고치기 완료. 페이지 이동만 설정했음.
 develop-0.1.7 : 2020-12-28 개발 설정 완료. 싱글 CRUD 시작 (mgr -> tea -> ... -> rep 순서)
 develop-0.1.8 : 2020-12-29 mariadb 설정
+develop-0.2.0 : 2020-12-29 Lambda Java Start
+develop-0.2.1 : 2020-12-29 강의자료 추가, Inventory.java 생성
